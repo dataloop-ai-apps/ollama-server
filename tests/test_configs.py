4fa8fd72da3de1ef3b35e73d028a63d45393a342
Pinhasi <yigal.pinhasi@dell.com>
2026-06-14 17:44:53 +0300

    update test cogi app_ids

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -6,8 +6,8 @@ class Environment(Enum):
 
 
 TEST_MODELS = [
-    {"model_name": "phi4-mini", 'app_id': "6a2ea553e1941e405f3d2d4d", "service_name": "ollama-service-phi-4-6a2ea553e194z"},  # new ollama phi4 app     
-    {"model_name": "gpt-oss:20b", 'app_id': "6a2eb4adb1b05b0a5bc9c885",  "service_name": "ollama-service-gpt-oss-20-b-6a2ebz"},  # new ollama gpt-oss-20b app
+    {"model_name": "phi4-mini", 'app_id': "6a2e4a09efa04c1a6267a096", "service_name": "ollama-service-phi4"},  # new ollama phi4 app     
+    {"model_name": "gpt-oss:20b", 'app_id': "6a2e407d2954388f96d61c6d",  "service_name": "ollama-service-gpt-oss"},  # new ollama gpt-oss-20b app
  ]
 
 RUN_ENV = Environment.PROD
